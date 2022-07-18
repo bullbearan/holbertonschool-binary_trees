3e3748146c4c16aa4a26adfd7f2d44c68dd2b0cb
bullbearan <bullbearan@gmail.com>
2022-07-18 22:53:10 +0000

    fix

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -21,6 +21,20 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	return (right_height + 1);
 }
 /**
+ *  check_leaf - Binary tree node
+ *
+ * @tree: Integer stored in the node
+ * Return: value
+ */
+int check_leaf(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	return (check_leaf(tree->left) && check_leaf(tree->right));
+}
+/**
  *  binary_tree_is_perfect - Binary tree node
  *
  * @tree: Integer stored in the node
@@ -28,9 +42,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	int n = 0, i;
+
 	if (tree == NULL)
 		return (0);
-	return (binary_tree_height(tree->left) == binary_tree_height(tree->right));
-	return (0);
-
+	n = check_leaf(tree->left) && check_leaf(tree->right);
+	i = binary_tree_height(tree->left) == binary_tree_height(tree->right) && n;
+	return (i);
 }
