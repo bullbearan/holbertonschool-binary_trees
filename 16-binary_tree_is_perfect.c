c7733d2b544685790ede20a571e2279781ee8404
bullbearan <bullbearan@gmail.com>
2022-07-20 09:08:38 +0100

    test

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -21,20 +21,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	return (right_height + 1);
 }
 /**
- *  check_leaf - Binary tree node
- *
- * @tree: Integer stored in the node
- * Return: value
- */
-int check_leaf(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
-	return (check_leaf(tree->left) && check_leaf(tree->right));
-}
-/**
  *  binary_tree_is_perfect - Binary tree node
  *
  * @tree: Integer stored in the node
@@ -42,13 +28,15 @@ int check_leaf(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int n = 0, i;
-
-	if (tree->left == NULL && tree->right == NULL && tree->parent == NULL)
-		return (1);
 	if (tree == NULL)
 		return (0);
-	n = check_leaf(tree->left) && check_leaf(tree->right);
-	i = binary_tree_height(tree->left) == binary_tree_height(tree->right) && n;
-	return (i);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	if (tree->left == NULL || tree->right == NULL)
+		return (0);
+if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
+{
+return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->left));
+}
+return (0);
 }
